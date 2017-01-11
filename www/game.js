7e9cfe25875c4ebb4a38ed1ed7f2da6cf5dd7bc1
gods1son <tolulopeibiyode@gmail.com>
2017-01-11 16:38:17 -0500

    new background

--- www/game.js
+++ www/game.js
@@ -219,3 +219,25 @@ function openLink(){
     counter = window.localStorage.getItem('key');
     window.open(faces[counter].link, '_blank', 'location=yes');
 }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+/* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
+function openAccord(){
+    var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
+}
+
