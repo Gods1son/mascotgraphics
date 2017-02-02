8ce03732a60a93946bda87b53db59b762383d0ea
gods1son <tolulopeibiyode@gmail.com>
2017-02-02 17:03:27 -0500

    video player

--- www/gametwo.js
+++ www/gametwo.js
@@ -410,6 +410,6 @@ function darktheme(){
 }
 function closevideo(){
     setTimeout(function(){
-        document.getElementById('videoholder').style.display = "none"
+        VideoPlayer.play("https://youtu.be/_xGhK6qgPtM");
     },14000);
 }
